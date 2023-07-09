2db4e78783f34afa284974a8f00e96a6f039430f
supriya <supriyack11@gmail.com>
2023-07-10 01:58:03 +0530

    Testng Project

---
+++ TestngAutomation/src/com/sgtesting/products/Products.java
@@ -0,0 +1,39 @@
+package com.sgtesting.products;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Products {
+	
+	@Test
+	public void createProduct()
+	{
+		System.out.println("The Product demoProduct1 has created successfully");
+	}
+
+	@Test
+	public void modifyProduct()
+	{
+		System.out.println("The Product demoProduct1 has modified successfully");
+	}
+	
+	@Test
+	public void deleteProduct()
+	{
+		System.out.println("The Product demoProduct1 has deleted successfully");
+	}
+	
+	@BeforeClass
+	public void setUp()
+	{
+		System.out.println("Launches Browser, Navigate the URL and Login into the Application");
+	}
+	@AfterClass
+	public void tearDown()
+	{
+		System.out.println("Logout from the Application and Closes the Application");
+		System.out.println("+++++++++++++++++++++++++++++++++");
+	}
+
+}
